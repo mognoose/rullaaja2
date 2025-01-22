1785aef5b12b4e669d1a7fa09150d305c7c365fc
sapatti <mognoose@gmail.com>
2025-01-22 22:30:53 +0200

    add database and list roll history

--- components/Roll.vue
+++ components/Roll.vue
@@ -3,8 +3,13 @@
     <input type="text" v-model="user" placeholder="name">
     <input type="text" v-model="dice" placeholder="dice">
     <input type="text" v-model="channel" placeholder="channel">
-    <button @click="testServer">ROLL</button>
+    <input type="checkbox" v-model="discord" id="discord" /><label for="discord">Post to Discord</label>
+    <button @click="roll">ROLL</button>
     {{ rnd }}
+    <hr>
+    <ul v-for="msg in messages?.rows">
+        <li>{{ msg.message }}</li>
+    </ul>
 </template>
 
 <script setup>
@@ -12,15 +17,22 @@ const config = useRuntimeConfig()
 const user = ref('Sapatti');
 const dice = ref(20);
 const channel = ref(config.public.DEFAULT_CHANNEL);
+const discord = ref(false)
+const messages = ref()
 
 const rnd = ref(0);
 
-const testServer = async () => {
+onMounted( async () => {
+    await updateMessages();
+})
+
+const roll = async () => {
     const options = {
         method: 'post',
         body: {
             user: user.value,
             dice: dice.value,
+            discord: discord.value,
             channel: channel.value,
         }
     }
@@ -29,5 +41,11 @@ const testServer = async () => {
     console.log(res);
     
     rnd.value = res.rnd;
+
+    updateMessages();
+}
+
+const updateMessages = async () => {
+    messages.value = await $fetch('/api/messages');
 }
 </script>
--- server/api/roll.post.ts
+++ server/api/roll.post.ts
@@ -4,11 +4,23 @@ client.login(process.env.CLIENT_SECRET);
 
 export default defineEventHandler(async event => {
 
+    const db = useDatabase();
+
+    await db.sql`CREATE TABLE IF NOT EXISTS messages ("id" TEXT PRIMARY KEY, "message" TEXT)`;
+
     const body = await readBody(event);
 
     const user = body.user || 'Anonymous';
     const channelId = body.channel || process.env.DEFAULT_CHANNEL;
     const dice = body.dice || 20;
+
+    const rnd = Math.ceil(Math.random()*dice);
+
+    const msg = `${user} heitti D${dice} nopalla: ${rnd}`;
+
+    await db.sql`INSERT INTO messages VALUES (${Date.now()}, ${msg})`;
+
+    if (!body.discord) return {msg, rnd};
 
     if (!channelId) {
         throw createError({
@@ -26,9 +38,7 @@ export default defineEventHandler(async event => {
         })
     }
 
-    const rnd = Math.ceil(Math.random()*dice);
 
-    const msg = `${user} heitti D${dice} nopalla: ${rnd}`;
 
     channel.send(msg);
 
