d78197d152939a6ff3346763505a5a7d8c9ec1a5
sapatti <mognoose@gmail.com>
2025-01-23 20:04:15 +0200

    remove database

--- components/Roll.vue
+++ components/Roll.vue
@@ -1,15 +1,11 @@
 <template>
-    <h1>test template</h1>
+    <h1>Rullaaja</h1>
     <input type="text" v-model="user" placeholder="name">
     <input type="text" v-model="dice" placeholder="dice">
     <input type="text" v-model="channel" placeholder="channel">
     <input type="checkbox" v-model="discord" id="discord" /><label for="discord">Post to Discord</label>
     <button @click="roll">ROLL</button>
     {{ rnd }}
-    <hr>
-    <ul v-for="msg in messages?.rows">
-        <li>{{ msg.message }}</li>
-    </ul>
 </template>
 
 <script setup>
@@ -18,13 +14,7 @@ const user = ref('Sapatti');
 const dice = ref(20);
 const channel = ref(config.public.DEFAULT_CHANNEL);
 const discord = ref(false)
-const messages = ref()
-
 const rnd = ref(0);
-
-onMounted( async () => {
-    await updateMessages();
-})
 
 const roll = async () => {
     const options = {
@@ -42,10 +32,6 @@ const roll = async () => {
     
     rnd.value = res.rnd;
 
-    updateMessages();
 }
 
-const updateMessages = async () => {
-    messages.value = await $fetch('/api/messages');
-}
 </script>
